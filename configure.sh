fa91d79bac8a0ff20e7098e6a8f5b2e7c62f5552
TokiZeng <gn02322440@gmail.com>
2024-10-26 19:25:55 +0800

    first commit

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
-extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
+extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -fslp-vectorize -fvectorize"
 
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mtune=cortex-a53" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mtune=cortex-a53  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
-
+./configure CXXFLAGS="--gcc-toolchain=/usr -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mcpu=cortex-a73" \
+CFLAGS="--gcc-toolchain=/usr -finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mcpu=cortex-a73 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize $extracflags" \
+CXX=clang++ CC=clang LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
